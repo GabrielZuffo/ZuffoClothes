9800c27a33822b1a3a450a80fab3b2654abbc8fa
zuffobotafogo <141636270+zuffobotafogo@users.noreply.github.com>
2025-04-24 21:00:39 -0300

    test 4

--- test/test.py
+++ test/test.py
@@ -1,21 +1,24 @@
-import pytest
-# Página inicial
 from src.app import *
+import pytest
+from flask import Flask, render_template, jsonify
+from flask_cors import CORS
 
-@pytest.fixture
-def app_context():
-    with app.app_context():
-        yield
-def test_home(app_context):
-    assert home() ==  render_template('home.html')
+app = Flask(__name__)
+CORS(app)
+# Página inicial
+
+def test_home():
+   with app.app_context():
+        rendered = render_template("home.html", name="Tester")
+        assert "Tester" in rendered 
 
 # Página da loja
 
-def test_loja(app_context):
+def test_loja():
      assert loja() == render_template('index.html')
 
 # ✅ Nova feature: Lista de camisas em JSON
-def test_listar_camisas(app_context):
+def test_listar_camisas():
     test_camisas = [
         {
             "id": 1,
